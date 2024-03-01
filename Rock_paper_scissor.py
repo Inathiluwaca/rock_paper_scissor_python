5b68b9140a3db81c92c03e9f3384adfd35463abf
Inathi <luwacainathi2@gmail.com>
2024-03-01 22:04:17 +0200

    halfway through hangman

--- Rock_paper_scissor.py
+++ Rock_paper_scissor.py
@@ -1,13 +1,55 @@
 import random
-def user_input():
+def options():
     choices=["rock","paper","scissors"]
+    return choices
+
+
+def user_input(choices):
     while True:
-        user=input("Insert your choice.\n")
-        if user not in choices:
+        user_choice=input("Insert your choice.\n").lower()
+        if user_choice not in choices:
             print("Invalid choice!")
         else:
-            return choices
+            return user_choice
+        
+def computer_choice(choice):
+    computer_choose=random.choice(choice)
+    print(f"Robo chose {computer_choose}.")
+    return computer_choose
 
 
+def run_choice(computer_choose,user_choice):
+    list_computer=[]
+    list_user=[]
+    win="You have won"
+    comp_win="Robo won."
+    if computer_choose=="paper" and user_choice == "scissor":
+        print(win)
+        list_computer.append(win)
+    elif computer_choose =="scissor" and user_choice == "rock":
+        print(win)
+        list_computer.append(win)
+    elif computer_choose== "rock" and  user_choice == "paper":
+        print(win)
+        list_computer.append(win)
+    if user_choice=="paper" and computer_choose== "scissor":
+        print(comp_win)
+        list_computer.append(win)
+    elif user_choice =="scissor" and computer_choose == "rock":
+        print(comp_win)
+        list_user=[]
+    elif user_choice== "rock" and  computer_choose == "paper":
+        print(comp_win)
+        list_user=[]
+    elif user_choice == computer_choose:
+        print("Draw")
+    print(list_user)
+        
+    
 if __name__=="__main__":
-    user_input()
+    for i in range(3):
+        choices=options()
+        user_choice=user_input(choices)
+        computer_choose =computer_choice(choices)
+        run_choice(computer_choose,user_choice)
+    
---
+++ hello.py
@@ -0,0 +1,7 @@
+x=input("num")
+if x.isalpha()==True:
+    print("letter")
+elif int(x)>10:
+    print('adult')
+else:
+    print("young")
