09cbe102e380587634a8a29b10af53dd7eba180a
Inathi <luwacainathi2@gmail.com>
2024-03-24 01:52:19 +0200

    fix bug

--- Rock_paper_scissor.py
+++ Rock_paper_scissor.py
@@ -1,6 +1,10 @@
 import random
+
+def welcome():
+    print("Welcome to the rock,paper scissors game. You have three tries.\nInsert quit if you want to end game.")
+
 def options():
-    choices=["rock","paper","scissors"]
+    choices=["rock","paper","scissor","quit"]
     return choices
 
 
@@ -12,44 +16,50 @@ def user_input(choices):
         else:
             return user_choice
         
-def computer_choice(choice):
-    computer_choose=random.choice(choice)
+def computer_choice():
+    computer_choose=["rock","paper","scissor"]
+    computer_choose=random.choice(computer_choose)
     print(f"Robo chose {computer_choose}.")
     return computer_choose
 
 
 def run_choice(computer_choose,user_choice):
-    list_computer=[]
-    list_user=[]
+  
     win="You have won"
     comp_win="Robo won."
-    if computer_choose=="paper" and user_choice == "scissor":
+    if user_choice == "quit":
+        print("Goodbye")
+        return True
+    elif computer_choose=="paper" and user_choice == "scissor":
         print(win)
-        list_computer.append(win)
+      
     elif computer_choose =="scissor" and user_choice == "rock":
         print(win)
-        list_computer.append(win)
+
     elif computer_choose== "rock" and  user_choice == "paper":
         print(win)
-        list_computer.append(win)
+      
     if user_choice=="paper" and computer_choose== "scissor":
         print(comp_win)
-        list_computer.append(win)
+      
     elif user_choice =="scissor" and computer_choose == "rock":
         print(comp_win)
-        list_user=[]
+      
     elif user_choice== "rock" and  computer_choose == "paper":
         print(comp_win)
-        list_user=[]
+ 
     elif user_choice == computer_choose:
         print("Draw")
-    print(list_user)
-        
-    
+    return False
+
 if __name__=="__main__":
+    welcome()
     for i in range(3):
         choices=options()
         user_choice=user_input(choices)
-        computer_choose =computer_choice(choices)
-        run_choice(computer_choose,user_choice)
-    
+        if user_choice == "quit":
+            break  
+        computer_choose =computer_choice()
+        if run_choice(computer_choose,user_choice):
+            break 
+    print("Game is over.")
--- hello.py
+++
@@ -1,7 +0,0 @@
-x=input("num")
-if x.isalpha()==True:
-    print("letter")
-elif int(x)>10:
-    print('adult')
-else:
-    print("young")
